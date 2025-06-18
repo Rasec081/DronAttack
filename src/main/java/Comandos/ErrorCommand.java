7e43cd21b9f722fb478bab768c5fb12e991c158d
Rasec081 <gamboacesar379@gmail.com>
2025-06-17 22:55:29 -0600

    Enviar clases

---
+++ src/main/java/Comandos/ErrorCommand.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Comandos;
+
+public class ErrorCommand extends BaseCommand {       
+    private static final String COMMAND_NAME = "ERROR";       
+    
+    @Override       
+    public String getCommandName() {           
+        return COMMAND_NAME;   
+    }       
+    
+    @Override       
+    public void execute(String[] args) {           
+        String message = "Error al invocar el comando";    
+    }   
+}
+
